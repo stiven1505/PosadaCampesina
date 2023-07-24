882ea34f5f920df27d9109ca67cf7c207e511e68
stiven1505 <83484277+stiven1505@users.noreply.github.com>
2023-07-24 10:27:22 -0500

    Botonera Arreglada

--- js/index.js
+++ js/index.js
@@ -1,38 +1,33 @@
-var switcher = document.getElementById('switcher');
-var switcher2 = document.getElementById('switcher2');
-var switcher3 = document.getElementById('switcher3');
-var switcher4 = document.getElementById('switcher4');
 
-switcher.addEventListener('click', function () {
-  if (this.checked) {
-    setTimeout(() => {
-      window.location.href = '../landing.html';
-    }, 1000);
-  }
-});
+    var switcher = document.getElementById('switcher');
+    var switcher2 = document.getElementById('switcher2');
+    var switcher3 = document.getElementById('switcher3');
+    var switcher4 = document.getElementById('switcher4');
 
-switcher2.addEventListener('click', function () {
-  if (this.checked) {
-    setTimeout(() => {
-      window.location.href = 'https://www.instagram.com/posadacampesinalagloria/';
-    }, 1000);
-  }
-});
+    switcher.addEventListener('click', function () {
+      setTimeout(() => {
+        window.location.href = '../landing.html';
+        switcher.checked = true;
+      }, 1000);
+    });
 
-switcher3.addEventListener('click', function () {
-  if (this.checked) {
-    setTimeout(() => {
-      window.location.href = 'https://www.youtube.com/@posadacampesinalagloria30';
-    }, 1000);
-  }
-});
+    switcher2.addEventListener('click', function () {
+      setTimeout(() => {
+        window.location.href = 'https://www.instagram.com/posadacampesinalagloria/';
+        switcher2.checked = true;
+      }, 1000);
+    });
 
-switcher4.addEventListener('click', function () {
-  if (this.checked) {
-    setTimeout(() => {
-      window.location.href =
-        'https://api.whatsapp.com/send?phone=573107437992&text=🏡%20Buenvenid%40s%20a%20Posada%20Capesina.Déjanos%20tu%20mensaje%20y%20en%20un%20momento%20nos%20comunicaremos%20contigo.%20Muchas%20gracias%20🤗¡Mi%20posada%20es%20tu%20posada!';
-    }, 1000);
-  }
-});
+    switcher3.addEventListener('click', function () {
+      setTimeout(() => {
+        window.location.href = 'https://www.youtube.com/@posadacampesinalagloria30';
+        switcher3.checked = true;
+      }, 1000);
+    });
 
+    switcher4.addEventListener('click', function () {
+      setTimeout(() => {
+        window.location.href = 'https://api.whatsapp.com/send?phone=573107437992&text=🏡%20Buenvenid%40s%20a%20Posada%20Capesina.Déjanos%20tu%20mensaje%20y%20en%20un%20momento%20nos%20comunicaremos%20contigo.%20Muchas%20gracias%20🤗¡Mi%20posada%20es%20tu%20posada!';
+        switcher4.checked = true;
+      }, 1000);
+    });
